83864854fdabf0b82a755495bb276e1598688d04
Tales Cione <talesj@gmail.com>
2018-12-08 00:36:09 -0200

    Solved the second puzzle from day 07 (somewhat poorly, but with the right answer :)

--- days/07/code.rb
+++ days/07/code.rb
@@ -8,7 +8,12 @@ def question_1
 end
 
 def question_2
-  p "question2: PENDING"
+  result = InstructionsParser
+           .time_to_complete(data_source: './input.txt',
+                             base_duration: 60,
+                             workers_count: 5)
+
+  p "question2: #{result}"
 end
 
 question_1
--- days/07/instructions_parser.rb
+++ days/07/instructions_parser.rb
@@ -1,6 +1,9 @@
 # Module made to order instructions given by a source file
 # I will try to use a more functional approach to this problem :)
-# It is important to state that this will not be purelly functional programming :o
+# It is important to state that this will not be purelly functional programming
+#
+# It is also importante to say that the solution ended up rather convoluted,
+# but I am pretty tired right now, so I'll call it a day and wrap this thing :)
 module InstructionsParser
   module_function
 
@@ -25,10 +28,16 @@ module InstructionsParser
     end
   end
 
-  def available_steps(stack)
+  def available_steps(stack, ignore: [])
     tally = tallied_stack(stack)
-    diff = tally[:shouldas].split('') - tally[:befores].split('')
+    diff = tally[:shouldas].split('') - tally[:befores].split('') - ignore
     diff.uniq.sort
+  end
+
+  def last_step(stack)
+    tally = tallied_stack(stack)
+    diff = tally[:befores].split('') - tally[:shouldas].split('')
+    diff.uniq.first
   end
 
   def next_step(stack)
@@ -66,22 +75,95 @@ module InstructionsParser
   end
 
   def time_for_task(task_name, base_duration)
-    index = ('a'..'z').to_a.find_index(task.name.downcase)
+    index = ('a'..'z').to_a.find_index(task_name.downcase)
     index + 1 + base_duration
+  end
+
+  def workable_steps(stack:,
+                     finished_steps:,
+                     work_in_progress:,
+                     l_step:,
+                     workers_count: )
+    a_steps = available_steps(stack, ignore: finished_steps)
+    steps_in_progress = work_in_progress.keys - finished_steps
+    pending_steps = a_steps - steps_in_progress
+
+    w_steps = [].concat(steps_in_progress, pending_steps)
+                .slice(0, workers_count)
+                .sort
+
+    w_steps.length == 0 ? [l_step] : w_steps
+  end
+
+  def update_work_in_progress(stack:,
+                              finished_steps:,
+                              work_in_progress:,
+                              l_step:,
+                              workers_count:,
+                              base_duration: )
+
+    w_steps = workable_steps(stack: stack,
+                             finished_steps: finished_steps,
+                             work_in_progress: work_in_progress,
+                             l_step: l_step,
+                             workers_count: workers_count)
+
+    w_steps.reduce(Hash.new(Float::INFINITY)) do |acc, value|
+      next acc if finished_steps.include?(value)
+
+      current_value = work_in_progress[value]
+      task_length = time_for_task(value, base_duration)
+
+      new_value = current_value > task_length ? task_length - 1 : current_value - 1
+
+      acc[value.freeze] = new_value
+      acc
+    end
   end
 
   def start_task_completion(workers_count:,
                             stack:,
                             base_duration:,
+                            l_step:,
                             finished_steps: [],
-                            work_in_progress: Hash.new(false),
+                            work_in_progress: Hash.new(Float::INFINITY),
                             duration: 0)
-    return duration if stack.length == 0
+    return duration if stack.length == 0 && finished_steps.include?(l_step)
+
+    updated_wip = update_work_in_progress(stack: stack,
+                                          finished_steps: finished_steps,
+                                          work_in_progress: work_in_progress,
+                                          l_step: l_step,
+                                          base_duration: base_duration,
+                                          workers_count: workers_count)
+
+    just_finished = updated_wip.keys.select do |value|
+      updated_wip[value] <= 0
+    end
+
+    newly_finished_steps = [].concat(finished_steps, just_finished)
+    popped_stack = stack.reject do |item|
+      newly_finished_steps.include?(item[:should_finish])
+    end
+
+    start_task_completion(workers_count: workers_count,
+                          stack: popped_stack,
+                          l_step: l_step,
+                          base_duration: base_duration,
+                          finished_steps: newly_finished_steps,
+                          work_in_progress: updated_wip,
+                          duration: duration + 1)
   end
 
   def time_to_complete(data_source:,
                        workers_count: 1,
                        base_duration: 0)
-    0
+    requirements_from_source(data_source)
+      .yield_self do |reqs|
+        start_task_completion(workers_count: workers_count,
+                              stack: reqs,
+                              l_step: last_step(reqs),
+                              base_duration: base_duration)
+      end
   end
 end
